b93a4a2b0013c619a7fe6a37c80f64ea6c6b21ac
Hans Jin <hans_jin@126.com>
2024-04-24 17:06:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ os.system(f'cd {base_path} && git lfs pull')
 
 backend_config = TurbomindEngineConfig(cache_max_entry_count=0.2,model_format="awq")
 
-pipe = pipeline(base_path,backend_config=backend_config)
+pipe = pipeline(base_path, backend_config=backend_config, model_name='internlm2-chat-1_8b-4bit')
 gen_config = GenerationConfig(top_p=0.8, top_k=40, temperature=0.8, max_new_tokens=1024)
 
 def chat(message,history):
-    response = pipe(message, model_name='internlm2-chat-1_8b-4bit', gen_config = gen_config)
+    response = pipe(message, gen_config = gen_config)
     return response.text
 
 gr.ChatInterface(chat,
